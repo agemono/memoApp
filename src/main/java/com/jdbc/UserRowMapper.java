ad2468b16cf8ea762099db88adad8e19feabe185
nakagoshi <smtwtfstffsow@gmail.com>
2021-04-14 18:27:09 +0900

    jdbc追加

--- src/main/java/com/jdbc/UserRowMapper.java
+++ src/main/java/com/jdbc/UserRowMapper.java
@@ -1,5 +1,28 @@
 package com.jdbc;
 
-public class UserRowMapper {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.user.User;
+
+
+public class UserRowMapper implements RowMapper<User> {
+
+	@Override
+	public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+		
+		//戻り値用のUserインスタンスの生成
+		User user = new User();
+		
+		//ResultSetの取得結果をUserインスタンスにせっと
+		
+		user.setUserid(rs.getString("user_id"));
+		user.setPassword(rs.getString("user_password"));
+		return user;
+	}
+	
+	
 
 }
